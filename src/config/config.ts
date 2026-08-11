74ac15ea93bcf53e1e16da2cf02cc698fe1edb77
KodirPro <soft_1c@mail.ru>
2026-08-11 21:04:10 +0500

    код 3

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
+    baseURL: import.meta.env.VITE_API_BASE_URL || "https://base.migjs.uz:40320/SML_SAP/hs",
 });
 
 api.interceptors.request.use((config) => {
