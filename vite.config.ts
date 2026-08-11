312a27d04b0f1eb15da4192b3e41c262d2154046
muhammadiso0203 <muhammadiso0203@gmail.com>
2026-08-11 13:37:24 +0500

    Update backend domain to base.migjs.uz

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://94.158.50.204:40318',
+        target: 'http://base.migjs.uz:40318',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
