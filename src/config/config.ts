dd7a7bb1ba08f8b8116aaed7cd02da7bd40c1f6d
KodirPro <soft_1c@mail.ru>
2026-08-14 11:04:04 +0500

    kod 10

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,25 +1,3 @@
-// import axios from "axios";
-
-// const api = axios.create({
-//     // baseURL: import.meta.env.VITE_API_BASE_URL || "https://base.migjs.uz:40320/SML_SAP/hs",
-//     baseURL: import.meta.env.VITE_API_BASE_URL
-// });
-
-// api.interceptors.request.use((config) => {
-//     const storedToken = localStorage.getItem("auth_token");
-//     if (storedToken) {
-//         config.headers.Authorization = `Basic ${storedToken}`;
-//     } else {
-//         const username = import.meta.env.VITE_API_USERNAME;
-//         const password = import.meta.env.VITE_API_PASSWORD;
-//         if (username && password) {
-//             config.headers.Authorization = `Basic ${btoa(`${username}:${password}`)}`;
-//         }
-//     }
-//     return config;
-// });
-
-// export default api;
 import axios from "axios";
 
 const api = axios.create({
